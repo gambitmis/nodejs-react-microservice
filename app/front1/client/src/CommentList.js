4ef51d8a2d906abbc1a2bc4f6fd9286b60d6aa56
gambitmis <gambitmis@gmail.com>
2021-08-06 18:07:56 +0000

    react finish

---
+++ app/front1/client/src/CommentList.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const CommentList = ({ postId }) => {
+    const [comments, setComments] = useState([]);
+    const fetchData = async () => {
+        const res = await axios.get(`http://localhost:3004/posts/${postId}/comments`);
+        //console.log(res.data)
+        setComments(res.data);
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const renderedComments = comments.map( comment => {
+        return <li key={comment.id}>{comment.content}</li>
+    });
+    return (
+        <ul>{renderedComments}</ul>
+    );
+};
+
+export default CommentList
+ 
