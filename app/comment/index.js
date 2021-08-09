bf590d165ac4a7b515877c21c73137c5fe4328bd
gambitmis <gambitmis@gmail.com>
2021-08-09 03:45:05 +0000

    update comment for v1

--- app/comment/index.js
+++ app/comment/index.js
@@ -20,14 +20,15 @@ app.post('/posts/:id/comments', async (req,res) => {
     const { content } = req.body;
 
     const comments = commentByPostId[req.params.id] || [];
-    comments.push({ id: commentId, content });
+    comments.push({ id: commentId, content, status:'pending' });
     commentByPostId[req.params.id] = comments;
     await axios.post('http://event-bus:4005/events',{
         type: 'CommentCreated',
         data: {
             id: commentId,
             content,
-            postId: req.params.id
+            postId: req.params.id,
+            status: 'pending'
         }
     });
     res.status(201).send(comments);
