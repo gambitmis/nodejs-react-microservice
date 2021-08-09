0288f013892777bb9daae16f710df88a2da312c9
gambitmis <gambitmis@gmail.com>
2021-08-09 03:00:30 +0000

    update mod

---
+++ app/moderation/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+const port = 4004
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post('/events', async (req,res) => {
+    const { type, data } = req.body;
+
+    if (type === 'CommentCreated') {
+        const status = data.content.include('Orange') ? 'rejected' : 'approved';
+        await axios.post('http://event-bus:4005/events', { 
+            type: 'commentModerated',
+            data: {
+                id: data.id,
+                postId: data.postId,
+                status,
+                content: data.content
+            }
+        });
+    }
+});
+
+app.listen(port,() => {
+    console.log(`Listening on port ${port}`);
+});
