09f20963ff39c9a7f0dff6e8e8d09c49c769a54a
gambitmis <gambitmis@gmail.com>
2021-08-10 07:15:56 +0000

    update filter

--- app/web/client/src/CommentList.js
+++ app/web/client/src/CommentList.js
@@ -3,7 +3,19 @@ import React from 'react';
 const CommentList = ({ comments }) => {
 
     const renderedComments = comments.map( comment => {
-        return <li key={comment.id}>{comment.content}</li>
+        let content;
+
+        if (comment.status === 'approved'){
+            content = comment.content;
+        } else if (comment.status === 'pending'){
+            content = 'This comment is awaitng moderation';
+        } else if (comment.status === 'rejected'){
+            content = 'This comment has been rejected';
+        } else {
+            content = 'ERROR'
+        }
+
+        return <li key={comment.id}>{content}</li>
     });
     return (
         <ul>{renderedComments}</ul>
